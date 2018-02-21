7d7211ecb469bf508f490137d93b9f30f3a42330
SunRuiXin <36697961+SunRuiXin@users.noreply.github.com>
2018-02-21 20:55:45 +0800

    亚马逊商品页面的爬取
     多增加了代理用户

---
+++ Fist-Chapter/test2.py
@@ -0,0 +1,14 @@
+import requests
+def getHTMLText(url):
+	try:
+		kv = {'user-agent' : 'Mozilla/5.0'}
+		r = requests.get(url, headers = kv, timeout = 30)
+		r.raise_for_status()
+		r.encoding = r.apparent_encoding
+		return r.text[:1000]
+	except:
+		return 'erro'
+
+if __name__ == '__main__':
+	url = 'https://www.amazon.cn/gp/product/B06X9H7FCC/ref=amb_link_5/461-3033461-8027158?pf_rd_m=A1AJ19PSB66TGU&pf_rd_s=merchandised-search-1&pf_rd_r=C8431JEXKZV4CXMGEKMN&pf_rd_r=C8431JEXKZV4CXMGEKMN&pf_rd_t=101&pf_rd_p=3c8c75b5-6d80-4f0d-ac97-39de59da54c0&pf_rd_p=3c8c75b5-6d80-4f0d-ac97-39de59da54c0&pf_rd_i=658390051'
+	print(getHTMLText(url))
